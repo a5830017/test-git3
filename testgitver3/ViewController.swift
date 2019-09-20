ca8a52e2c570efc91eec70820d71a7503a022e01
Jiratip <a5830017a@gmail.com>
2019-09-20 15:14:27 +0700

    first commit

---
+++ testgitver3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testgitver3
+//
+//  Created by Jiratip Hemwutthipan on 20/9/2562 BE.
+//  Copyright © 2562 Jiratip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
